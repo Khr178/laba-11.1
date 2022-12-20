16a825456fe85e3bffd63040297efbd95a998806
Khrystyna <hrustunakopeika2005@gmail.com>
2022-12-20 18:00:04 +0200

    first master commit

---
+++ юніт тест 11.1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace юніттест111
+{
+	TEST_CLASS(юніттест111)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int main();
+			char* CreateBIN; "enter file name 1";
+			char* PrintBIN = "enter file name 2";
+		}
+	};
+}
